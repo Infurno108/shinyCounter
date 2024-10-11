43fff4c0299f48172ff9dad396f12bd9a61421f9
Flint Rose <oliverflintrose@gmail.com>
2024-10-10 21:39:36 -0500

    Rounded the %s

--- script.js
+++ script.js
@@ -5,24 +5,27 @@ function loadData() {
     .then((data) => {
       const hunt = data.hunt;
       count = data.count;
+      let math = (Math.pow(1364 / 1365, count) * 100).toFixed(2);
+      let inverse = (100 - math).toFixed(2);
+
       document.getElementById("hunt").innerHTML = "Currently hunting: " + hunt;
       document.getElementById("count").innerHTML = "Count: " + count;
-      let math = Math.pow(1364 / 1365, count) * 100;
       document.getElementById("math").innerHTML =
         math + "% of people are still hunting";
       document.getElementById("math2").innerHTML =
-        100 - math + "% of people got the shiny";
+        inverse + "% of people got the shiny";
     });
 }
 
 function sr() {
   count = count + 1;
-  let math = Math.pow(1364 / 1365, count) * 100;
+  let math = (Math.pow(1364 / 1365, count) * 100).toFixed(2);
+  let inverse = (100 - math).toFixed(2);
   document.getElementById("math").innerHTML =
     math + "% of people are still hunting";
   document.getElementById("count").innerHTML = "Count: " + count;
   document.getElementById("math2").innerHTML =
-    100 - math + "% of people got the shiny";
+    inverse + "% of people got the shiny";
 }
 
 window.onload = loadData;

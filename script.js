61c515e8dbc00e92d8d90596fb1a7e40fa0402f1
Flint Rose <oliverflintrose@gmail.com>
2024-10-16 10:35:49 -0500

    stopping the example

--- script.js
+++ script.js
@@ -3,12 +3,6 @@
 var config = {
   hunts: [
     {
-      hunt: "Zygarde",
-      count: 0,
-      method: "Soft Reset",
-      probability: 0,
-    },
-    {
       hunt: "Reshiram",
       count: 1180,
       method: "Soft Reset",

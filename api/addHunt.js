3f415298c9e037ec404610f7506629ce489afb9f
Flint Rose <oliverflintrose@gmail.com>
2024-10-30 16:38:47 -0500

    edited api

--- api/addHunt.js
+++ api/addHunt.js
@@ -7,7 +7,8 @@ export default async function handler(request, response) {
     const count = req.query.count;
     const probability = req.query.probability;
     const have = req.query.have;
-    if (!petName || !ownerName) throw new Error("Pet and owner names required");
+    if (!hunt || !method || !count || !probability || !have)
+      throw new Error("Input required");
     await sql`INSERT INTO hunt (hunt, method, count, probability, have) VALUES (${hunt}, ${method}, ${count}, ${probability}, ${have});`;
   } catch (error) {
     return response.status(500).json({ error });
